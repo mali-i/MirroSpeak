7b97017c0708b7ce333321e8027e974b8c0b3ba2
mali-i <hello_mali_i@163.com>
2026-06-24 15:51:57 +0800

    构建版本改为3...

--- forge.config.js
+++ forge.config.js
@@ -8,7 +8,7 @@ const platformFromArgs = platformArgIndex >= 0
 const forgePlatform = process.env.FORGE_PLATFORM || process.env.npm_config_platform || platformFromArgs;
 const isMasBuild = forgePlatform === 'mas';
 const forgeOutDir = process.env.FORGE_OUT_DIR;
-const buildVersion = process.env.APP_BUILD_NUMBER || '1';
+const buildVersion = process.env.APP_BUILD_NUMBER || '3';
 const signingIdentity = isMasBuild
   ? process.env.APPLE_MAS_CERTIFICATE_IDENTITY
   : process.env.APPLE_CERTIFICATE_IDENTITY;
